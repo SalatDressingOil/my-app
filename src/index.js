e53a6c84ebee6c565cffd4eb96c059d0ba4a857e
Алексей <@DESKTOP-D98N22D>
2022-10-26 13:46:33 +0300

    firstcommit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,114 @@
+import React from "react";
+import ReactDOM, { render } from "react-dom/client";
+import {
+  AdaptivityProvider,
+  ConfigProvider,
+  useAdaptivity,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  ViewWidth,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  SimpleCell,
+  PanelHeaderBack,
+  Cell,
+  CellButton
+} from "@vkontakte/vkui";
+import "@vkontakte/vkui/dist/vkui.css";
+
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activePanel: "panel1",
+    };
+  }
+
+  render() {
+    return (
+      <View activePanel={this.state.activePanel}>
+        <Panel id="panel1">
+          <PanelHeader>More</PanelHeader>
+          <Group>
+            <Cell
+              expandable
+              onClick={() => this.setState({ activePanel: "panel2" })}
+            >
+              Friends
+            </Cell>
+            <Cell
+              expandable
+              onClick={() => this.setState({ activePanel: "panel2" })}
+            >
+              Communities
+            </Cell>
+            <Cell
+              expandable
+              onClick={() => this.setState({ activePanel: "panel2" })}
+            >
+              Music
+            </Cell>
+          </Group>
+        </Panel>
+        <Panel id="panel2">
+          <PanelHeader
+            separator={false}
+            before={
+              <PanelHeaderBack
+                onClick={() => this.setState({ activePanel: "panel1" })}
+              />
+            }
+          >
+            Communities
+          </PanelHeader>
+          <Group>
+            <Cell
+              description="Humor"
+              onClick={() => this.setState({ activePanel: "panel3" })}
+            >
+              Swipe Right
+            </Cell>
+            <Cell
+              description="Cultural Center"
+              onClick={() => this.setState({ activePanel: "panel3" })}
+            >
+              Out Cinema
+            </Cell>
+            <Cell
+              description="Movies"
+              onClick={() => this.setState({ activePanel: "panel3" })}
+            >
+              #ARTPOKAZ
+            </Cell>
+          </Group>
+        </Panel>
+        <Panel id="panel3" centered>
+          <PanelHeader
+            before={
+              <PanelHeaderBack
+                onClick={() => this.setState({ activePanel: "panel2" })}
+              />
+            }
+          >
+            Out Cinema
+          </PanelHeader>
+          <div style={{ marginTop: 10 }}>Centered Content</div>
+        </Panel>
+      </View>
+    );
+  }
+}
+
+<Example />;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Example />);
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +126,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
